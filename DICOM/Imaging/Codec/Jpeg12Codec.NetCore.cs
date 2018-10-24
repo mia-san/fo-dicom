64a90e3466d3873726534d7e326a44e78af18b21
mia-san <mercenary_mia@hotmail.com>
2018-10-24 19:41:46 +0900

    add libijg12 bridge class

--- DICOM/Imaging/Codec/Jpeg12Codec.NetCore.cs
+++ DICOM/Imaging/Codec/Jpeg12Codec.NetCore.cs
@@ -3,68 +3,16 @@
     /// <summary>
     ///
     /// </summary>
-    public class Jpeg12Codec : IJpegNativeCodec
+    public class Jpeg12Codec : JpegCodecBase
     {
-        /// <summary>
-        ///
-        /// </summary>
-        public JpegMode Mode { get; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        public int PointTransform { get; }
-
-        /// <summary>
-        ///
-        /// </summary>
-        public int Predictor { get; }
-
         /// <summary>
         ///
         /// </summary>
         /// <param name="mode"></param>
         /// <param name="predictor"></param>
         /// <param name="pointTransform"></param>
-        public Jpeg12Codec(JpegMode mode, int predictor, int pointTransform)
+        public Jpeg12Codec(JpegMode mode, int predictor, int pointTransform): base(mode, predictor, pointTransform, 12)
         {
-            this.Mode = mode;
-            this.PointTransform = pointTransform;
-            this.Predictor = predictor;
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="oldPixelData"></param>
-        /// <param name="newPixelData"></param>
-        /// <param name="jpegParams"></param>
-        /// <param name="frame"></param>
-        public void Decode(NativePixelData oldPixelData, NativePixelData newPixelData, NativeJpegParameters jpegParams, int frame)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="oldPixelData"></param>
-        /// <param name="newPixelData"></param>
-        /// <param name="jpegParams"></param>
-        /// <param name="frame"></param>
-        public void Encode(NativePixelData oldPixelData, NativePixelData newPixelData, NativeJpegParameters jpegParams, int frame)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="pixelData"></param>
-        /// <returns></returns>
-        public int ScanHeaderForPrecision(NativePixelData pixelData)
-        {
-            throw new System.NotImplementedException();
         }
     }
 }
