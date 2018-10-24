07b4b5bab1180e2f9f7157d6af17005db5c5ae3a
mia-san <mercenary_mia@hotmail.com>
2018-10-24 19:30:43 +0900

    add unmanaged interface for libijg16

---
+++ Native/NetCore/libijg16.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <setjmp.h>
+#include "../libijg16/jinclude16.h"
+#include "../libijg16/jpeglib16.h"
+
+#include "cscontext.h"
+
+#include "cscontext.i"
+#include "csdecode.i"
