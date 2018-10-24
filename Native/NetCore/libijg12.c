cc13364c0797eb1fb4c285f957d8cd4947e269cd
mia-san <mercenary_mia@hotmail.com>
2018-10-24 19:30:32 +0900

    add unmanaged interface for libijg12

---
+++ Native/NetCore/libijg12.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <setjmp.h>
+#include "../libijg12/jinclude12.h"
+#include "../libijg12/jpeglib12.h"
+
+#include "cscontext.h"
+
+#include "cscontext.i"
+#include "csdecode.i"
