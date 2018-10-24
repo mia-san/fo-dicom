b9517453baa39bc14e45bf9071cce24ae59d9b08
mia-san <mercenary_mia@hotmail.com>
2018-10-24 19:30:22 +0900

    add unmanaged interface for libijg8

---
+++ Native/NetCore/libijg8.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <setjmp.h>
+#include "../libijg8/jinclude8.h"
+#include "../libijg8/jpeglib8.h"
+
+#include "cscontext.h"
+
+#include "cscontext.i"
+#include "csdecode.i"
